614cfce9c6eac841ee03c48a690f95f782745005
khoa <khoanguyen0791@gmail.com>
2020-02-02 10:54:45 -0600

    Multi adding, multi-adding.

--- python/multiprocessing_queue.py
+++ python/multiprocessing_queue.py
@@ -13,30 +13,33 @@ class TestMulti:
         while q.qsize() < limit:
             num = random.random()
             q.put(num)
+            print(f"Putting")
+            time.sleep(0.5)
 
     def consumer(self, q: Queue):
-        if q.qsize() != 0:
-            print(q.get())
+        while True:
+            print(f"Getting: {q.get()}")
+            time.sleep(0.5)
 
     def run(self):
         # pool = Pool(cpu_count())
         q: Queue = Queue()
-        limit: int = 999
+        limit: int = 20
         process_generates = [Process(target=self.producer_add, args=(q, 999)) for i in range(cpu_count())]
         process_prints = [Process(target=self.consumer, args=(q,)) for i in range(cpu_count())]
         for p in process_generates:
             p.start()
 
         # time.sleep(5)
-        for p in process_generates:
-            p.join()
 
         print(q.qsize(), len(process_generates))
 
-        # for pr in process_prints:
-        #     pr.start()
-        # for pr in process_prints:
-        #     pr.join()
+        for pr in process_prints:
+            pr.start()
+        for p in process_generates:
+            p.join()
+        for pr in process_prints:
+            pr.join()
 
         print(q.qsize(), len(process_prints))
 
