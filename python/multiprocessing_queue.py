1929b2cfcdb657cfc4768d8c5b7aea884af50a01
khoa <khoanguyen0791@gmail.com>
2020-02-02 11:05:37 -0600

    Figured out how to terminate queue.

--- python/multiprocessing_queue.py
+++ python/multiprocessing_queue.py
@@ -13,31 +13,31 @@ class TestMulti:
         while q.qsize() < limit:
             num = random.random()
             q.put(num)
-            print(f"Putting")
-            time.sleep(0.5)
+            print(f"Q size {q.qsize()}")
+        q.put(None)
 
     def consumer(self, q: Queue):
         while True:
-            print(f"Getting: {q.get()}")
-            time.sleep(0.5)
+            temp = q.get()
+            if temp is None:
+                break
+            print(f"Getting: {temp}")
 
     def run(self):
         # pool = Pool(cpu_count())
         q: Queue = Queue()
         limit: int = 20
-        process_generates = [Process(target=self.producer_add, args=(q, 999)) for i in range(cpu_count())]
+        process_generates = [Process(target=self.producer_add, args=(q, limit)) for i in range(cpu_count())]
         process_prints = [Process(target=self.consumer, args=(q,)) for i in range(cpu_count())]
         for p in process_generates:
             p.start()
 
-        # time.sleep(5)
-
         print(q.qsize(), len(process_generates))
-
-        for pr in process_prints:
-            pr.start()
         for p in process_generates:
             p.join()
+        for pr in process_prints:
+            pr.start()
+
         for pr in process_prints:
             pr.join()
 
